ebab9f79526748dd4b4f7b08658edf6094434203
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2022-07-23 13:27:00 +0200

    Update Project.r

--- Project.r
+++ Project.r
@@ -1,6 +1,5 @@
 # R Project for Monitoring Ecosystems
 
-# With this project I want to highligt the differences in Snow Cover Extent in the region where I live between the winter 2020/2021 and winter 2021/2022
 
 # setting the working directory
 setwd("/Users/magalicorti/Desktop/project/")
@@ -15,8 +14,12 @@ library(viridis)       # to use viridis palette
 library(patchwork)     # to plot together plots made with ggplot
  
 
+
+##### With this project I want to highligt the differences in Snow Cover Extent in the Cetral Alps in Nortern Itlay between the winter 2021 and winter 2022 ##### 
+
 # SCE - Snow Cover Extent
 # Copernicus data with geometric resolution of 500m x 500m per pixel
+
 # importing Copernicus data for winter 2022
 snow20220125 <- raster("c_gls_SCE500_202201250000_CEURO_MODIS_V1.0.1.nc")
 # visualizing the image imported
@@ -38,11 +41,11 @@ p21 <- ggplot() + geom_raster(snow21, mapping = aes(x=x, y=y, fill = Snow.Cover.
 p22 <- ggplot() + geom_raster(snow22, mapping = aes(x=x, y=y, fill = Snow.Cover.Extent)) + scale_fill_viridis() + ggtitle("Snow Cover in winter 2022")
 
 # to visualize the two plots together in a vertical sequence
-# if I wanted to visualize them in an horizontal sequence Ishuold have used + instead of /
+# if I wanted to visualize them in an horizontal sequence I shuold have used + instead of /
 p21 / p22
 
 
-# otherwise to import multiple data with with the same pattern in the name I can create a list and use the lapply function
+# otherwise to import multiple data with the same pattern in the name I can create a list and use the lapply function
 rlist <- list.files(pattern = "SCE")
 rlist # list of 2 images
 list_rast <- lapply(rlist, brick)   # can also use raster function 
@@ -60,7 +63,6 @@ cldif = colorRampPalette(c("blue", "white", "red"))(100)
 plot(SCEdif, col=cldif) # in blue the snow missing in 2022 with respect to 2021
 
 
-
 # qualitative analysis of proportions and frequency distributions
 # plotting frequency distribution of snow cover values -> plot all histograms together
 par(mfrow=c(1,2))
@@ -68,13 +70,13 @@ hist(snow21, xlim = c(0,200))
 hist(snow22, xlim = c(0,200))
 
 
-# PERCHE DISTRIBUZIONE DEI. DATI LUNGO LINEE?? GUARDO MAPPE
+# PERCHE DISTRIBUZIONE DEI. DATI LUNGO LINEE?? GUARDO MAPPE -> risoluzione, cropping ??
 # plotting values of 2022 against 2021
 # comparing data one in function of the other
 plot(snow21, snow22, xlab = "Snow Cover Extent in 2021", ylab = "Snow Cover Extent in 2022") 
 abline(0, 1, col="red") # plotting line, making it passing trough 0
 
-# plotting automatically all graphs together, very usefull when we have many graphs
+# plotting automathically all graphs together, very usefull when we have many graphs
 pairs(snowstack)
 
 
@@ -89,7 +91,7 @@ plot(s21$map)
 # computing the frequency for each class
 freq(s21$map)
 total <- 360000 # tot amount of pixels -> run s21 -> look at tird value of dimension (ncell)
-# compute percentage per type of cover (frequency of class / total)
+# compute proportion per type of cover (frequency of class / total)
 propsnow21 <- 239706/total # class 2
 propbare21 <- 94315/total # class 1
 propwater21 <- 25979/total # class 3
@@ -105,9 +107,8 @@ proportion21 <- data.frame(cover, prop21) # proportion of pixels in 2021
 proportion21 # quantitative data
 
 # plotting data with ggplot2
-# ggplot function -> first argument = dataset, other arguments = aesthetic, color stored in cover
 # geom_bar function explainig type of graph
-# stat - statistics used, identity bc we're using data as they are (no median or mean)
+# stat - statistics used = identity because we're using data as they are (no median or mean)
 # for changing limit from 0 to 1 use ylim()
 PR21 <- ggplot(proportion21, aes(x=cover, y=prop21, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1)
 
@@ -120,7 +121,7 @@ plot(snow22)
 
 freq(s22$map)
 total <- 360000
-# compute percentage per type of cover (frequency of class / total)
+# compute proportion per type of cover (frequency of class / total)
 propsnow22 <- 118301/total # class 3
 propbare22 <- 176219/total # class 1
 propwater22 <- 65480/total # class 2
@@ -136,6 +137,72 @@ grid.arrange(PR21, PR22, nrow=1)
 
 
 
+##### Now let's see if we can see differences in the temperature of the lakes present in the region #####
+
+# LSWT - Lake Surface Water Temperature
+# Copernicus data with geometric resolution of 1000m x 1000m per pixel
+
+# importing Copernicus data for land surface temperature winter 2021
+lswt2021 <- raster("c_gls_LSWT_202103010000_GLOBE_SLSTRAB_v1.1.0.nc")
+# cropping the image focusing on the same area of interest
+lswt21 <- crop(lswt2021, ext)
+plot(lswt21)
+
+# importing Copernicus data for land surface temperature winter 2021
+lswt2022 <- raster("c_gls_LSWT_202203010000_GLOBE_SLSTRAB_v1.1.0.nc")
+# cropping the image focusing on the same area of interest
+lswt22 <- crop(lswt2022, ext)
+plot(lswt22)
+
+par(mfrow=c(2,1)) 
+plot(lswt21)
+plot(lswt22)
+
+# new cropping focusing on the lake area
+ext1 <- c(8, 12, 45, 46.5)
+lswt2021 <- raster("c_gls_LSWT_202103010000_GLOBE_SLSTRAB_v1.1.0.nc")
+# cropping the image focusing on the same area of interest
+lswt21 <- crop(lswt2021, ext1)
+plot(lswt21)
+
+# importing Copernicus data for land surface temperature winter 2021
+lswt2022 <- raster("c_gls_LSWT_202203010000_GLOBE_SLSTRAB_v1.1.0.nc")
+# cropping the image focusing on the same area of interest
+lswt22 <- crop(lswt2022, ext1)
+plot(lswt22)
+
+par(mfrow=c(2,1)) 
+plot(lswt21, main = "Lake Surface Water Temperature in 2021")
+plot(lswt22, main = "Lake Surface Water Temperature in 2022")
+
+# using ggplot function with viridis (changing colorRampPalette)
+lt21 <- ggplot() + geom_raster(lswt21, mapping = aes(x=x, y=y, fill = lake.surface.skin.temperature)) + scale_fill_viridis() + ggtitle("Lake Surface Water Temperature in 2021")
+lt22 <- ggplot() + geom_raster(lswt22, mapping = aes(x=x, y=y, fill = lake.surface.skin.temperature)) + scale_fill_viridis() + ggtitle("Lake Surface Water Temperature in 2022")
+
+# to visualize the two plots together in a vertical sequence
+lt21 / lt22
+
+# computing differece in lake surface temperature between 2021 and 2022
+LSWTdif <- (lswt22 - lswt21)
+plot(LSWTdif)
+# changing color palette for a more clear visualization
+cldif = colorRampPalette(c("blue", "white", "red"))(100)
+plot(LSWTdif, col=cldif) # in red the higest temperature in 2022 with respect to 2021
+# let's use ggplot
+ldif22 <- ggplot() + geom_raster(LSWTdif, mapping = aes(x=x, y=y, fill = layer))+ scale_fill_viridis(option="magma") + ggtitle("Difference in Lake Surface Water Temperature")
+
+# plotting frequency distribution histogrames
+par(mfrow=c(1,2))
+hist(lswt21, xlim = c(275,285), ylim = c(0,450))
+hist(lswt22, xlim = c(275,285), ylim = c(0,450))
+
+# PERCHE DISTRIBUZIONE DEI. DATI LUNGO LINEE?? GUARDO MAPPE -> risoluzione, cropping ??
+# plotting values of 2022 against 2021
+# comparing data one in function of the other
+plot(lswt21, lswt22, xlab = "Lake Surface Water Temperature in 2021", ylab = "Lake Surface Water Temperature in 2022", xlim = c(275,285), ylim = c(275,285)) 
+abline(0, 1, col="red") # plotting line, making it passing trough 0
+
+
 
 
 
@@ -146,47 +213,42 @@ grid.arrange(PR21, PR22, nrow=1)
 # SWI messeggio di warning NON VA BENE
 
 
-
 # LST (bassa qualità immagine)
 # importing Copernicus data for land surface temperature winter 2021
 lst2021 <- raster("c_gls_LST_202101181400_GLOBE_GEO_V2.0.1.nc")
 # cropping the image focusing on the same area of interest
 lst21 <- crop(lst2021, ext)
 plot(lst21)
-
 # importing Copernicus data for land surface temperature winter 2021
 lst2022 <- raster("c_gls_LST_202202051400_GLOBE_GEO_V2.0.1.nc")
 # cropping the image focusing on the same area of interest
 lst22 <- crop(lst2022, ext)
 plot(lst22)
 
-par(mfrow=c(1,2)) # strani e unità di misura diverse
+par(mfrow=c(2,1)) # strani e unità di misura diverse
 plot(lst21)
 plot(lst22)
+
 dev.off()
 
 
-
-
 # NDVI da errore nel caricare il file!!!
+setwd("/Users/magalicorti/Desktop/ndvi/")
 # importing Copernicus data for land surface temperature winter 2021
 ndvi2021 <- raster("c_gls_NDVI300_202103210000_GLOBE_OLCI_V2.0.1.nc")
 # cropping the image focusing on the same area of interest
 ndvi21 <- crop(ndvi2021, ext)
 plot(ndvi21)
-
 # importing Copernicus data for land surface temperature winter 2021
 ndvi2022 <- raster("c_gls_NDVI300_202203210000_GLOBE_OLCI_V2.0.1.nc")
 # cropping the image focusing on the same area of interest
 ndvi22 <- crop(ndvi2022, ext)
 plot(ndvi22)
 
-par(mfrow=c(1,2)) # strani e unità di misura diverse
+par(mfrow=c(1,2))
 plot(ndvi21)
 plot(ndvi22)
 dev.off()
-
-
 
 
 
